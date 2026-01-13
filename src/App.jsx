91fe8d440f9d3da8966cfbbd62101b7f21e5197a
diwash1437 <diwashlimbu47@gmail.com>
2026-01-14 02:31:04 +0900

    add deploy script

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Restaurant from "./Recipe/Restaurant";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Restaurant />} />
+      <Route path="/hontenss/" element={<Restaurant />} />
       {/* <Route path="/nabata" element={<NabataColtd153 />} /> */}
     </Routes>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/honten_restaurant_website">
+  <BrowserRouter basename="/hontenss/">
     <App />
   </BrowserRouter>
 )
